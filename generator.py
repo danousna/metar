6e5a04b05f136c5acdaa23e643fcf8d9b308463a
Corentin Hembise <corentinhembise@free.fr>
2019-06-13 11:06:29 +0200

    Add generator

--- generator.py
+++ generator.py
@@ -2,12 +2,17 @@ import csv
 import re
 import glob, os
 
-def loadata():
+def loadata(limit = False):
+    i = 0
     os.chdir("data")
-    for file in glob.glob("*.csv"):
+    for file in glob.glob("*.txt"):
         with open(file) as f:
             for row in csv.DictReader(f):
-                yield row
+                if not limit or i < limit:
+                    i += 1
+                    yield row
+                else:
+                    break
 
-for r in loadata():
+for r in loadata(300):
     print(r)
