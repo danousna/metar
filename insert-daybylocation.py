27b2e65d03c5fd839f4ed764d6ab335ec0d296f6
Corentin Hembise <corentinhembise@free.fr>
2019-06-15 16:11:59 +0200

    Insert data

--- insert-daybylocation.py
+++ insert-daybylocation.py
@@ -23,7 +23,7 @@ ks = c.connect('chembise_metar_1_12')
 data = generator.loadata(100)
 
 for row in data:
-    print(format_insert_query("date_by_location", data, {
+    print(format_insert_query("date_by_location", row, {
         'valid': 'daytime'
     }))
 
