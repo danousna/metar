f7f5de465dfbf0cd4faf223a0feb60e3d0635529
Corentin Hembise <corentinhembise@free.fr>
2019-06-15 16:45:33 +0200

    Wrong folder

--- insert-data.py
+++ insert-data.py
@@ -1,12 +1,15 @@
 import cassandra.cluster
 import generator
+from cql_tils import format_insert_query
 
 c = cassandra.cluster.Cluster(['localhost'])
 ks = c.connect('chembise_metar_1_12')
 
-data = generator.loadata(100)
+data = generator.loadata(300)
 
 for row in data:
-    print(format_insert_query("date_by_location", row, {
-        'valid': 'daytime'
-    }))
+    query = format_insert_query("date_by_location", row, {
+        'valid': 'daytime',
+    })
+
+    ks.execute(query)
