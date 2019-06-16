73335608c6e581b229365d31f3bfdec0c577f15c
Natan Danous <natan.danous@gmail.com>
2019-06-16 11:57:29 +0200

    Basic graph

--- question_1.py
+++ question_1.py
@@ -1,23 +1,31 @@
 import matplotlib.pyplot as plt
-from fixtures import data
+import numpy
+from datetime import datetime
+from cassandra.cluster import Cluster
 
-plt.plot([1, 2, 3, 4, 5], [1, 1, 10, 4, 5])
-plt.ylabel('some numbers')
+
+def get_points(c, year):
+    query = "SELECT month, day, AVG(tmpf) AS tmpf FROM date_by_location WHERE lat = 52.5644 AND lon = 13.3088 AND year = {} GROUP BY month, day;".format(year)
+    print(query)
+    results = c.execute(query)
+
+    data = [numpy.nan for i in range(365)]
+
+    for result in results:
+        day = datetime.strptime(str(year) + '-' + str(result.month) + '-' + str(result.day), '%Y-%m-%d').timetuple().tm_yday
+        data[day] = result.tmpf
+
+    return data
+
+
+cluster = Cluster(['localhost'])
+cluster = cluster.connect('chembise_metar_1_12')
+
+data = get_points(cluster, 2011)
+
+for i in range(365):
+    print(str(i) + ' : ' + str(data[i]))
+
+plt.scatter(data)
+plt.xticks(rotation = 90)
 plt.savefig('temperatures.png')
-
-def get_points(station, year = '2001'):
-    return {
-        'temperature': [],
-        'day': []
-    }
-
-def avg_temperatures():
-    years = ['2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010']
-
-    avg_points = {
-        'temperature': [],
-        'day': []
-    }
-
-    for year in years:
-        points = get_points(year)
